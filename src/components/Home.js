6e32e228889f4799162e25d132ebbf20507a381b
root <root@DESKTOP-BEUDVQH.localdomain>
2020-12-04 11:09:43 -0800

    Start landing page and initial app setup

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react"
+import '../App.css';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1 className="profile-information">
+      Kelsi Proulx
+      </h1>
+    </div>
+  );
+}
