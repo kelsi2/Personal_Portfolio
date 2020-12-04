1dfdfe960256ca40b3a36dac903b415d3e311e02
root <root@DESKTOP-BEUDVQH.localdomain>
2020-12-04 15:50:43 -0800

    Progress on landing page layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,23 @@
 import React from "react"
 import '../App.css';
+import {Link} from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className="container">
-      <h1 className="profile-information">
-      Kelsi Proulx
-      </h1>
+  <>
+    <div className="canvas">
+      <div id="container">
+          <h1 className="name">Kelsi <br/> Proulx</h1>
+          <h1 className="title">Full Stack <br/> Web Developer</h1>
+      </div>
+      <span id="btn-container">
+        <Link to="#" className="learn-more">
+          Find out More
+          <br/>
+          <i className="fas fa-angle-double-right"></i>
+        </Link>
+      </span>
     </div>
+  </>
   );
 }
